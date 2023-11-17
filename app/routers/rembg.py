b1f70f40e59b92c561537b44a8ca4a0e3a538df8
Onekki <zhuwangqi@foxmail.com>
2023-11-17 16:26:47 +0800

    ✨ feat(single): return image bytes

--- app/routers/rembg.py
+++ app/routers/rembg.py
@@ -1,6 +1,4 @@
-from typing import Annotated
-
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 from pydantic import BaseModel
 
 import rembg
@@ -16,9 +14,7 @@ router = APIRouter(
 @router.post("/single")
 async def single(image: Image):
     response = requests.get(image.url)
-    with open('output.png', 'wb') as o:
-        model_name = "isnet-general-use"
-        session = rembg.new_session(model_name)
-        output = rembg.remove(response.content, session=session)
-        o.write(output)
-        return image
+    model_name = "isnet-general-use"
+    session = rembg.new_session(model_name)
+    bytes = rembg.remove(response.content, session=session)
+    return Response(content=bytes, media_type='image/png')
